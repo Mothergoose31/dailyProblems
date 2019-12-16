684858fadc2aa125c5544919c5763cb3d0d909b5
Mothergoose31 <JosephAlvarengaBeech@protonmail.com>
2019-12-16 11:01:10 -0800

    typedArray

--- Javascript/ZigZag.js
+++ Javascript/ZigZag.js
@@ -1,6 +1,17 @@
 
-// Logic Behind ZigZag Can Be found in the ZigZagLogicGraph
+// A 'zig-zag' array is a square arrangement of the first  N2  integers, where the numbers increase sequentially as you zig-zag along the array's anti-diagonals.
 
+// For example, for the input 5, the following result should be produced:
+
+//  0  1  5  6 14
+//  2  4  7 13 15
+//  3  8 12 16 21
+//  9 11 17 20 22
+// 10 18 19 23 24
+// Write a function that takes the size of the zig-zag matrix, and 
+// returns the corresponding matrix as two-dimensional array.
+
+// Logic Behind ZigZag Can Be found in the ZigZagLogicGraph.png
 
 function zigZagMatrix(n){
     var result = new Array(n)
