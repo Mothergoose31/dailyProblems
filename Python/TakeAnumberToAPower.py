ffd36b98d11399a792b0e33deef6a9fef8bd2ac0
Nata <JosephAlvarengaBeech@protonmail.com>
2020-04-15 11:22:19 -0700

    numberToAPower

---
+++ Python/TakeAnumberToAPower.py
@@ -0,0 +1,16 @@
+# Implement pow(x, n), which calculates x raised to the power n (xn).
+# sincer  raising an integer to a power is the same as multiplying it n times 
+#  it first solution would be to multiply x , n number of times until n reaches 1
+
+
+# def takeANumToAPow(base,power):
+#     h = power
+#     xOne=base
+#     xTwo=base
+#     while(h > 0):
+#         xOne = xOne*xTwo
+#         h = h-1
+#     print(xOne)
+# takeANumToAPow(2,9)
+
+# this was a simple first iteration solution but what if power is negative 🤔
